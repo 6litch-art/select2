72b6f7733fcaf09235e868779bd5b920ace5be28
Marco Meyer (www) <marco.meyerconde@gmail.com>
2022-11-30 08:58:58 +0100

    Update, cleanup

--- src/index.js
+++ src/index.js
@@ -1,10 +1,5 @@
 
 import 'select2/dist/js/select2.full.js'
-require('jquery-ui/ui/widget')
-require('jquery-ui/ui/scroll-parent')
-require('jquery-ui/ui/data')
-require('jquery-ui/ui/widgets/mouse')
-require('jquery-ui/ui/ie')
 require('jquery-ui/ui/widgets/sortable')
 
 import './css/index.css';
